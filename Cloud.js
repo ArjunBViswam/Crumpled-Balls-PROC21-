a88bb4deb8f7cd4d4934644734bd550394886bf3
Arjun Biju Viswam <82167383+ArjunBViswam@users.noreply.github.com>
2021-05-30 22:33:04 +0530

    Update Cloud.js

--- Cloud.js
+++ Cloud.js
@@ -6,7 +6,7 @@ class Cloud{
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
         this.height = height
-        this.image = loadImage("image/cloud.png");
+        this.image = loadImage("cloud.png");
 
         World.add(world,this.body);
     }
